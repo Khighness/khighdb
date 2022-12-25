2b1d6a6ddf11dc7a6883ea42280505f233c2373f
cantai <cantai@pinduoduo.com>
2022-12-25 22:54:52 +0800

    Data Structure: zset coding.

--- data/zset/zset.go
+++ data/zset/zset.go
@@ -1,4 +1,72 @@
 package zset
 
+import (
+	"github.com/khighness/khighdb/storage"
+	"github.com/khighness/khighdb/util"
+)
+
 // @Author KHighness
 // @Update 2022-12-25
+
+const (
+	maxLevel    = 32
+	probability = 0.25
+)
+
+type EncodeKey func(key, subKey []byte) []byte
+
+type (
+	// SortedSet defines the structure of sorted set.
+	SortedSet struct {
+		record map[string]*SortedSetNode
+	}
+
+	// SortedSetNode defines the structure of sorted set node.
+	SortedSetNode struct {
+		dict map[string]*sklNode
+		skl  *skipList
+	}
+)
+
+type (
+	sklLevel struct {
+		forward *sklNode
+		span    uint64
+	}
+
+	sklNode struct {
+		member   string
+		score    float64
+		backward *sklNode
+		level    []*sklLevel
+	}
+
+	skipList struct {
+		head   *sklNode
+		tail   *sklNode
+		length int64
+		level  int16
+	}
+)
+
+// New creates a new sorted set.
+func New() *SortedSet {
+	return &SortedSet{
+		record: make(map[string]*SortedSetNode),
+	}
+}
+
+func (z *SortedSet) IterateAndSend(chn chan *storage.LogEntry, encode EncodeKey) {
+	for key, ss := range z.record {
+		zsetKey := []byte(key)
+		if ss.skl.head == nil {
+			return
+		}
+		for e := ss.skl.head.level[0].forward; e != nil; e = e.level[0].forward {
+			scoreBuf := []byte(util.Float64ToStr(e.score))
+			encKey := encode(zsetKey, scoreBuf)
+			chn <- &storage.LogEntry{Key: encKey, Value: []byte(e.member)}
+		}
+	}
+	return
+}
---
+++ util/str.go
@@ -0,0 +1,26 @@
+package util
+
+import "strconv"
+
+// @Author KHighness
+// @Update 2022-12-25
+
+// Float64ToStr converts type float64 to string.
+func Float64ToStr(val float64) string {
+	return strconv.FormatFloat(val, "f", -1, 64)
+}
+
+// StrToFloat64 converts type string to float64.
+func StrToFloat64(val string) (float64, error) {
+	return strconv.ParseFloat(val, 64)
+}
+
+// StrToInt64 converts type string to int64.
+func StrToInt64(val string) (int64, error) {
+	return strconv.ParseInt(val, 10, 64)
+}
+
+// StrToUint converts type string to uint64.
+func StrToUint(val string) (uint64, error) {
+	return strconv.ParseUint(val, 10, 64)
+}
