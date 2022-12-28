c44e0e0a9db27720751a5256ef8e6ba5f566b7c4
cantai <cantai@pinduoduo.com>
2022-12-28 13:55:34 +0800

    Logger: use zap as default log.

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/khighness/khighdb
 go 1.16
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/plar/go-adaptive-radix-tree v1.0.5
-	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0
 	github.com/stretchr/testify v1.8.1
+	go.uber.org/zap v1.24.0
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ logger/zap.go
@@ -0,0 +1,71 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// @Author KHighness
+// @Update 2022-12-28
+
+// InitLogger initializes zap.Logger then we can use  zap.L() to get this logger.
+func InitLogger(level zapcore.Level) {
+	var core zapcore.Core
+	fileCore := zapcore.NewCore(zapFileEncoder(), zapWriteSyncer(), zapLevelEnabler())
+	consoleCore := zapcore.NewCore(zapConsoleEncoder(), os.Stdout, level)
+	core = zapcore.NewTee(fileCore, consoleCore)
+	logger := zap.New(core, zap.AddCaller())
+	zap.ReplaceGlobals(logger)
+}
+
+func zapLevelEnabler() zapcore.Level {
+	return zapcore.InfoLevel
+}
+
+func zapEncodeConfig() zapcore.EncoderConfig {
+	return zapcore.EncoderConfig{
+		MessageKey:       "msg",
+		LevelKey:         "level",
+		TimeKey:          "ts",
+		NameKey:          "logger",
+		CallerKey:        "caller_line",
+		FunctionKey:      zapcore.OmitKey,
+		StacktraceKey:    "stacktrace",
+		LineEnding:       "\n",
+		EncodeLevel:      zapEncodeLevel,
+		EncodeTime:       zapcore.ISO8601TimeEncoder,
+		EncodeDuration:   zapcore.MillisDurationEncoder,
+		EncodeCaller:     zapEncodeCaller,
+		ConsoleSeparator: " ",
+	}
+}
+
+func zapFileEncoder() zapcore.Encoder {
+	return zapcore.NewJSONEncoder(zapEncodeConfig())
+}
+
+func zapConsoleEncoder() zapcore.Encoder {
+	return zapcore.NewConsoleEncoder(zapEncodeConfig())
+}
+
+func zapEncodeLevel(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + level.CapitalString() + "]")
+}
+
+func zapEncodeCaller(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(caller.TrimmedPath())
+}
+
+func zapWriteSyncer() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./log/khighdb.log",
+		MaxSize:    1000,
+		MaxBackups: 10,
+		MaxAge:     30,
+		Compress:   true,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
